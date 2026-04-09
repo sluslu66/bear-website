688f5485ba0dac9e10a76f1af761d405da191a3d
sluslu66 <gudhalism@gmail.com>
2026-04-09 09:48:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function showFact() {
-  const facts = [
-    "Beruang bisa berlari hingga 40 km/jam",
-    "Beruang memiliki penciuman sangat tajam",
-    "Beruang kutub adalah perenang hebat"
-  ];
-  
-  const random = facts[Math.floor(Math.random() * facts.length)];
-  document.getElementById("fact").innerText = random;
+const facts = [
+"Bears can run up to 40 km/h",
+"Bears have a very keen sense of smell",
+"Polar bears are excellent swimmers"
+];
+
+const random = facts[Math.floor(Math.random() * facts.length)];
+document.getElementById("fact").innerText = random;
 }
